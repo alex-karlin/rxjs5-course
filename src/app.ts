3bcaf9de32d747e0415b07ab197742c22cb54aa7
Alex Karlin <akarlin@boardbooks.com>
2017-04-24 10:14:32 +1200

    Implemented remote call to fetch movie data and render on the page

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,40 @@
-alert("Ok");
+import { Observable } from "rxjs";
+
+const output = document.getElementById("output");
+const button = document.getElementById("button");
+
+function load(url: string) {
+    return Observable.defer(() => {
+        return Observable.fromPromise(
+            fetch(url).then(response => response.json()))
+            .retryWhen(retryStrategy({ attempts: 3, delay: 1500 }));
+    });
+}
+
+function retryStrategy({ attempts = 4, delay = 1000 }) {
+    return function (errors) {
+        return errors
+            .scan((acc, value) => {
+                console.log(acc, value);
+                return acc + 1;
+            }, 0)
+            .takeWhile(acc => acc < attempts)
+            .delay(delay);
+    }
+}
+
+function renderMovies(movies) {
+    movies.forEach(movie => {
+        const div = document.createElement("div");
+        div.innerText = `${movie.id} - ${movie.title}`;
+        output.appendChild(div);
+    });
+}
+
+Observable.fromEvent(button, "click")
+    .flatMap(e => load("api/movies.json"))
+    .subscribe(
+        movies => renderMovies(movies),
+        error => console.log(`error: ${error}`),
+        () => console.log("complete")
+    );
